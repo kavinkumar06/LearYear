ac949c95379473e0adbb6d63f956081005bf1fdd
kavinkumar06 <kavinm09@gmail.com>
2017-07-11 10:21:46 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,26 @@
+# LearYear
+import java.util.*;
+public class LeapYear
+{
+   public static void main(String args[])
+   {
+     int year=2000;
+     boolean isLeapYear=false;
+     if(year%400==0)
+     {
+       isLeapYear=true;
+     }
+     else if(year%100==0)
+     {
+       isLeapYear=false;
+     }
+     if(isLeapYear)
+     {
+       System.out.println("Year "+year+" is a Leap Year");
+     }
+     else
+     {
+        System.out.println("Year "+year+" is a Leap Year");
+        }
+       }
+     }  
